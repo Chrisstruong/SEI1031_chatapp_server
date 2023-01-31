9800de687766416ef4e7ff17e32e7d5ae5d6a3fe
chrisstruong <chrisstruong@gmail.com>
2023-01-31 16:15:01 -0500

    Work on fake authentication so as to chat

--- index.js
+++ index.js
@@ -4,33 +4,33 @@ const {PORT, MONGODB_URI} = process.env
 const express = require("express")
 
 const app = express()
-const http = require('http')
+// const http = require('http')
 const cors = require('cors')
 const morgan = require("morgan")
-const {Server} = require("socket.io")
+// const {Server} = require("socket.io")
 
 app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
-const server = http.createServer(app)
+// const server = http.createServer(app)
 
-const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:localhost3000',
-        method: ["GET", "POST"],
-    }
-})
+// const io = new Server(server, {
+//     cors: {
+//         origin: 'http://localhost:localhost3000',
+//         method: ["GET", "POST"],
+//     }
+// })
 
-io.on("connection", (socket)=> {
-    console.log(`User connected: ${socket.id}`);
-    socket.on("join_room",(data)=>{
-        socket.join(data)
-        console.log(`user with ID: ${socket.id} joined room: ${data}`)
-    })
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected", socket.id)
-    })
-})
+// io.on("connection", (socket)=> {
+//     console.log(`User connected: ${socket.id}`);
+//     socket.on("join_room",(data)=>{
+//         socket.join(data)
+//         console.log(`user with ID: ${socket.id} joined room: ${data}`)
+//     })
+//     socket.on("disconnect",()=>{
+//         console.log("User Disconnected", socket.id)
+//     })
+// })
 
 
 
@@ -53,7 +53,6 @@ app.use('/conversations', conversationController)
 app.use('/messages', messageController)
 
 
-
-server.listen(4000, ()=>{
-    console.log("Server running")
+app.listen(PORT, ()=> {
+    console.log(`listening on: ${PORT}`)
 })
